bbb02f74ef43829196b1e4d49989027a420a9c2a
Juanlu <juanluiscas@gmail.com>
2022-02-14 18:50:32 +0100

    home and activities working

---
+++ pages/[id]/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import dbConnect from '../../lib/dbConnect'
+import Activity from '../../models/Activities.model'
+
+export default function Actividad({activity}) {
+  return (
+    <div>
+        <div className="max-w-md overflow-hidden">
+        <img src={activity.imgUrl} alt={activity.title} />
+        
+        <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{activity.title}</div>
+            <p className="text-gray-700 text-base">
+            {activity.description}
+            </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+	try {
+		await dbConnect()
+
+        const activity = await Activity.findById(params.id).lean()
+        activity._id = `${activity._id}`
+		return { props: { success: true,  activity } }
+	} catch (error) {
+		console.log(error)
+	}
+}
