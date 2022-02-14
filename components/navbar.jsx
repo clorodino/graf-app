569dd801c77262b381b64d29ac968b29bed606cc
Juanlu <juanluiscas@gmail.com>
2022-02-14 20:10:13 +0100

    activity details and navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
+import { HiOutlineMenu } from "react-icons/hi"
+import logoHome from '../public/logo_graf.svg'
+import Image from 'next/image'
 
-export default function Navbar() {
+
+function navbar() {
   return (
-    <div>navbar</div>
+    <nav className="container flex justify-between items-center px-4 py-2 mx-auto bg-slate-300">
+        <div>
+            <Link href='/' passHref>
+                <Image src={logoHome} alt='Logo Graf' width={110} height={60} />
+            </Link>
+        </div>
+        <div className="hidden space-x-8 lg:flex">
+            <a href="#">Actividades</a>
+            <a href="#">Mapa</a>
+            <a href="#">Firmas</a>
+            <a href="#">FAQ</a>
+        </div>
+        <div className="flex">
+            <HiOutlineMenu size={25} />
+        </div>
+    </nav>
   )
 }
+
+export default navbar
--- pages/[id]/index.jsx
+++ pages/[id]/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import dbConnect from '../../lib/dbConnect'
 import Activity from '../../models/Activities.model'
+import Navbar from '../../components/Navbar'
+import Link from 'next/link'
 
 export default function Actividad({activity}) {
   return (
     <div>
+        <Navbar />
         <div className="max-w-md overflow-hidden">
-        <img src={activity.imgUrl} alt={activity.title} />
-        
+         <img src={activity.imgUrl} alt={activity.title} />
         <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{activity.title}</div>
             <p className="text-gray-700 text-base">
@@ -15,9 +17,9 @@ export default function Actividad({activity}) {
             </p>
         </div>
         <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            <span className="inline-block bg-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span className="inline-block bg-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span className="inline-block bg-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
         </div>
         </div>
     </div>
