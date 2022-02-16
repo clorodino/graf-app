29d5cbb9d8a9aec8314f9b8d58dfa22a18ad646c
Juanlu <juanluiscas@gmail.com>
2022-02-16 11:43:42 +0100

    activity details page layout complete

--- pages/[id]/index.jsx
+++ pages/[id]/index.jsx
@@ -3,25 +3,49 @@ import dbConnect from '../../lib/dbConnect'
 import Activity from '../../models/Activities.model'
 import Navbar from '../../components/Navbar'
 import Link from 'next/link'
+import { HiOutlineLocationMarker, HiOutlineClock, HiOutlineCalendar } from "react-icons/hi";
 
 export default function Actividad({activity}) {
+
+  const day = { weekday: 'long',  month: 'long', day: 'numeric',minimumIntegerDigits: 2 };
+  const hour =`${activity.date.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2})}:${activity.date.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2})} h`
+
   return (
     <div>
+      
         <div className='fixed top-0 w-full max-w-md z-10'><Navbar /></div>
         <div className="max-w-md overflow-hidden flex flex-col relative top-20 mb-8 bg-white min-h-screen ">
          <img src={activity.imgUrl} alt={activity.title} />
         <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{activity.title}</div>
-            <div className="font-bold text-xl mb-2">{activity.guests}</div>
-            <p className="text-gray-700 font-semibold">{activity.location}</p>
-            <p className="text-gray-700 text-base">{activity.description}</p>
+            <div className="font-bold text-2xl mb-2">{activity.title}</div>
+            <div className="font-semibold text-xl text-slate-500 mb-2">con {activity.guests}</div>
+            <div className='flex justify-between pt-3'>
+              <div className='flex items-center'>
+                <HiOutlineCalendar className='color-icons'/>
+                <p className="text-sm text-slate-400 font-medium mx-1">{activity.date.toLocaleDateString('es-ES', day)}</p>
+              </div>
+              <div className='flex items-center'>
+                <HiOutlineClock className='color-icons'/>
+                <p className="text-sm text-slate-400 font-medium mx-1">{hour}</p>
+              </div>
+              <div className='flex items-center'>
+                <HiOutlineLocationMarker className='color-icons'/>
+                <p className="text-sm text-slate-400 font-medium mx-1">{activity.location}</p>
+              </div>
+            </div>
+            <hr className='my-5 border border-slate-300' />
+
+
+            
+            <p className="text-slate-700 text-base">{activity.description}</p>
+            
         </div>
-        <div className="px-6 pt-4 pb-2">
+        {/* <div className="px-6 pt-4 pb-2">
             <span className="inline-block bg-slate-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
             <span className="inline-block bg-slate-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
             <span className="inline-block bg-slate-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
             
-        </div>
+        </div> */}
         </div>
     </div>
   )
